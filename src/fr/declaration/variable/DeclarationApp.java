2fedd86c425a87a4b64686045555bd56523d660d
Damien Castello <d.castello.13200@gmail.com>
2026-06-22 12:07:45 +0200

    master(tp3): test imperative approach

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,25 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+        byte departement = 13;
+        short rue = 151;
+        int postalCode = 13200;
+        long humans = 6500000;
+        float money = 41.25f;
+        double temperature = 27.8;
+        char FirstCharHello = 'h';
+        boolean boolYes = true;
+        String mozinorVideo = "toto story";
+
+        System.out.println(mozinorVideo);
+
+        String randomString = "Voici le résultat d’un calcul : 1+5=6";
+
+        System.out.println(randomString);
+
+        randomString = "Voici le résultat d’un calcul \n: 1+5=6";
+
+        System.out.println(randomString);
+    }
+}
